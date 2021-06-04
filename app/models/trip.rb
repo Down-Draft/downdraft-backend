f5ffac5f399bea04254c482a273564b4eb534b7b
Joey Haas <75232735+joeyh92989@users.noreply.github.com>
2021-06-04 16:58:17 -0600

    Rubocop ran

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,8 +2,8 @@ class Trip < ApplicationRecord
   has_many :trip_beers
   has_many :beers, through: :trip_beers
 
-  validates_presence_of :date, :elevation, :name, :location, :user_id
+  validates :date, :elevation, :name, :location, :user_id, presence: true
   validates :user_id, numericality: true
-  validates :user_id, numericality: {greater_than: 0}
+  validates :user_id, numericality: { greater_than: 0 }
   validates :elevation, numericality: true
 end
--- app/services/elevation_service.rb
+++ app/services/elevation_service.rb
@@ -1,11 +1,10 @@
 class ElevationService
   def self.conn
-    Faraday.new("https://api.opentopodata.org")
+    Faraday.new('https://api.opentopodata.org')
   end
 
   def self.fetch_elevation(lat, long)
     response = conn.get("/v1/srtm90m?locations=#{lat},#{long}")
     JSON.parse(response.body, symbolize_names: true)[:results].first
-
-  end 
+  end
 end
