06a7e153544d0f57acd4b71334cd3119532c66e2
Joey Haas <75232735+joeyh92989@users.noreply.github.com>
2021-06-05 15:24:07 -0600

    ran and fixed rubocop errors

--- app/controllers/api/v1/elevation_controller.rb
+++ app/controllers/api/v1/elevation_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::ElevationController < ApplicationController
   def index
     if Geocoder.search(params[:location]) == []
-      render json: { errors: "Location not found" }, status: :not_found
+      render json: { errors: 'Location not found' }, status: :not_found
     else
       coordinates = Geocoder.search(params[:location]).first.coordinates
       elevation = ElevationService.fetch_elevation(coordinates[0], coordinates[1])
