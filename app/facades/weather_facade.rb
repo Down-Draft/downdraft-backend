b25df78dbb710fe25ca4efac277fe23e352d5dfd
Joey Haas <75232735+joeyh92989@users.noreply.github.com>
2021-06-07 10:14:08 -0600

    Refactored single day forecast facade method:

--- app/facades/weather_facade.rb
+++ app/facades/weather_facade.rb
@@ -14,13 +14,9 @@ class WeatherFacade
 
   def self.one_day_forecast(location, date)
     json = WeatherService.fetch_single_day(location, date)
-    location_name = json[:location][:name]
-    location_region = json[:location][:region]
-    json[:forecast][:forecastday].map do |day|
-      day[:location_name] = location_name
-      day[:location_region] = location_region
-      WeatherDay.new(day)
-    end.first
+    json[:forecast][:forecastday].first[:location_name] = json[:location][:name]
+    json[:forecast][:forecastday].first[:location_region] = json[:location][:region]
+    WeatherDay.new(json[:forecast][:forecastday].first)
   rescue NoMethodError
     json
   end
