a3507b0c13650f202e5d578dd189d7e2fbd57541
Joey Haas <75232735+joeyh92989@users.noreply.github.com>
2021-06-07 10:15:25 -0600

    removed set id from trip serializer

--- app/serializers/trip_serializer.rb
+++ app/serializers/trip_serializer.rb
@@ -1,6 +1,5 @@
 class TripSerializer
   include FastJsonapi::ObjectSerializer
-  set_id :object_id
   attributes :location, :name, :date, :elevation, :user_id
   has_many :beers, if: Proc.new{|record| record.beers.any?}
 end
