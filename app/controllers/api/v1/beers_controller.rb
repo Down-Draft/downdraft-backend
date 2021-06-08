6276d06ac409a4154ad7f3948710c25a744e9b3a
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-08 18:39:42 -0500

    Finish model methods for finding beers by temp and elevation

--- app/controllers/api/v1/beers_controller.rb
+++ app/controllers/api/v1/beers_controller.rb
@@ -1,9 +1,14 @@
 class Api::V1::BeersController < ApplicationController
   def index
-    #search by temp and elevation, happens when "Submit new trip" on /trips/new
-    trip = Trip.find_by_id(params[:trip_id])
-    if trip.max_temperature <= 80.0..61.0
+    trip = Trip.create!(date: "2021-06-08", elevation: 7, name: "Happy Trip", location: 33617, user_id: 1, max_temperature: 100)
+    if trip.max_temperature > 80.0
+      beers = Beer.recommended_beers_hot(trip.elevation)
+    elsif trip.max_temperature <= 80.0..61.0
       beers = Beer.recommended_beers_warm(trip.elevation)
+    elsif trip.max_temperature <= 61.0..32.0
+      beers = Beer.recommended_beers_cool(trip.elevation)
+    else
+      beers = Beer.recommended_beers_cold(trip.elevation)
     end
   end
 end
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -2,22 +2,52 @@ class Beer < ApplicationRecord
   belongs_to :style
   has_many :trip_beers, dependent: :destroy
   has_many :trips, through: :trip_beers
-
-  def self.find_beers(name)
-    where("lower(name) ILIKE '%#{name}%'").order(:name).limit(10)
-  end
-
-  def self.recommended_beers_warm(elevation)
-    if elevation >= 8000
-      set_abv = 5.6
-    elsif elevation < 8000 && elevation >= 5000
-      set_abv = 8
-    else
-      set_abv = 100
+  class << self
+    def find_beers(name)
+      where("lower(name) ILIKE '%#{name}%'").order(:name).limit(10)
     end
-    require "pry"; binding.pry
-    joins(:style).
-    where('style.style_name' => warm)
-    where("abv <= ?", "#{set_abv}")
+
+    def recommended_beers_hot(elevation)
+      beers = joins(:style).
+      where('styles.style_name ILIKE ANY(ARRAY[?])', hot).
+      where('abv < ?', set_abv(elevation)).
+      order("RANDOM()").
+      limit(9)
+      require "pry"; binding.pry
+    end
+
+    def recommended_beers_warm(elevation)
+      joins(:style).
+      where('styles.style_name ILIKE ANY(ARRAY[?])', warm).
+      where('abv < ?', set_abv(elevation)).
+      order("RANDOM()").
+      limit(9)
+    end
+
+    def recommended_beers_cool(elevation)
+      joins(:style).
+      where('styles.style_name ILIKE ANY(ARRAY[?])', cool).
+      where('abv < ?', set_abv(elevation)).
+      order("RANDOM()").
+      limit(9)
+    end
+
+    def recommended_beers_cold(elevation)
+      joins(:style).
+      where('styles.style_name ILIKE ANY(ARRAY[?])', cold).
+      where('abv < ?', set_abv(elevation)).
+      order("RANDOM()").
+      limit(9)
+    end
+
+    def set_abv(elevation)
+      if elevation >= 8000
+        5.6
+      elsif elevation < 8000 && elevation >= 5000
+        8
+      else
+        100
+      end
+    end
   end
 end
