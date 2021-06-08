f93e6cdbbeb855ab829ee75e14c2880201c43796
Klaudia Stewart <klaudiastewart@gmail.com>
2021-06-08 15:45:40 -0600

    Add max_temp param

--- app/controllers/api/v1/trips_controller.rb
+++ app/controllers/api/v1/trips_controller.rb
@@ -2,6 +2,8 @@ class Api::V1::TripsController < ApplicationController
   # if we do an update action on this controller don't forget to add to set_trip before action block
   before_action :set_trip, only: %i[show destroy]
   before_action :set_elevation, only: %i[create]
+  before_action :set_temperature, only: %i[create]
+
   def create
     trip = Trip.new(trip_params)
     if trip.save
@@ -35,7 +37,7 @@ class Api::V1::TripsController < ApplicationController
   private
 
   def trip_params
-    params.permit(:location, :name, :date, :user_id, :elevation)
+    params.permit(:location, :name, :date, :user_id, :elevation, :max_temperature)
   end
 
   def set_trip
@@ -46,6 +48,14 @@ class Api::V1::TripsController < ApplicationController
     coordinates = Geocoder.search(params[:location]).first.coordinates
     elevation = ElevationService.fetch_elevation(coordinates[0], coordinates[1])
     params[:elevation] = (elevation[:elevation] * 3.28084)
+  rescue NoMethodError
+    render json: { errors: 'Could not find location. Please ensure zip code is valid or try again later.' },
+           status: :bad_request
+  end
+
+  def set_temperature
+    max_temp = WeatherFacade.one_day_forecast(params[:location], params[:date])
+    params[:max_temperature] = max_temp.max_temp
   rescue NoMethodError
     render json: { errors: 'Could not find location. Please ensure zip code is valid or try again later.' },
            status: :bad_request
