8b9c2d7410a740f434bb2ef5820d4810a35a242c
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-08 20:02:03 -0500

    Remove trip creation from controller

--- app/controllers/api/v1/beers_controller.rb
+++ app/controllers/api/v1/beers_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::BeersController < ApplicationController
   def index
-    trip = Trip.create!(date: "2021-06-08", elevation: 7, name: "Happy Trip", location: 33617, user_id: 1, max_temperature: 100)
     if trip.max_temperature > 80.0
       beers = Beer.recommended_beers_hot(trip.elevation)
     elsif trip.max_temperature <= 80.0..61.0
