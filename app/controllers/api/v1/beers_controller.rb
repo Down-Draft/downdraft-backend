a670f70f4fe7473bb92f14caff4937c3c4861050
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-06-08 20:50:52 -0500

    Create tests for beer index

--- app/controllers/api/v1/beers_controller.rb
+++ app/controllers/api/v1/beers_controller.rb
@@ -1,13 +1,19 @@
 class Api::V1::BeersController < ApplicationController
   def index
-    if trip.max_temperature > 80.0
-      beers = Beer.recommended_beers_hot(trip.elevation)
-    elsif trip.max_temperature <= 80.0..61.0
-      beers = Beer.recommended_beers_warm(trip.elevation)
-    elsif trip.max_temperature <= 61.0..32.0
-      beers = Beer.recommended_beers_cool(trip.elevation)
+    trip = Trip.find_by_id(params[:trip_id])
+    if trip.present?
+      if trip.max_temperature > 80.0
+        beers = Beer.recommended_beers_hot(trip.elevation)
+      elsif trip.max_temperature <= 80.0..61.1
+        beers = Beer.recommended_beers_warm(trip.elevation)
+      elsif trip.max_temperature <= 61.0..32.0
+        beers = Beer.recommended_beers_cool(trip.elevation)
+      else
+        beers = Beer.recommended_beers_cold(trip.elevation)
+      end
+      render json: BeerSerializer.new(beers)
     else
-      beers = Beer.recommended_beers_cold(trip.elevation)
+      render json: { errors: 'No trip id entered' }, status: :bad_request
     end
   end
 end
--- spec/requests/api/v1/beers_index_spec.rb
+++ spec/requests/api/v1/beers_index_spec.rb
@@ -7,16 +7,25 @@ RSpec.describe 'API V1 Beers Controller', type: :request do
       @style1 = Style.create(style_name: "Lager")
       @style2 = Style.create(style_name: "Wheat")
       @style3 = Style.create(style_name: "Stout")
-      @beer1 = create(:beer, style: @style1)
+      @beer1 = create(:beer, style: @style1, abv: 3)
       @beer2 = create(:beer, style: @style2, abv: 2)
       @beer3 = create(:beer, style: @style2, abv: 10)
-      @beer4 = create(:beer, style: @style3)
+      @beer4 = create(:beer, style: @style3, abv: 11)
     end
 
     it 'can render beers based on a trips elevation and max temperature' do
       get '/api/v1/beers', params: {trip_id: "#{@trip.id}"}
-      #only beer 2
-      # beers = JSON.parse(response.body, symbolize_names: true)
+      beers = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(beers[:data].count).to eq(1)
+      expect(beers[:data].first[:attributes][:name]).to eq(@beer2.name)
+    end
+
+    it 'renders a 400 page if trip id does not exist' do
+      get '/api/v1/beers', params: {trip_id: 0}
+      expect(response.status).to eq(400)
+      expect(response.body).to eq("{\"errors\":\"No trip id entered\"}")
     end
   end
 end
